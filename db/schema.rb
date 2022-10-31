2da717fe563684d7d7c3d67c6bd08c21c4c5ef1b
Matt <matt.titmarsh@gmail.com>
2022-10-31 14:40:57 +1000

    Remove user_id from comptetitions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_31_033443) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_31_041520) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_31_033443) do
     t.string "title"
     t.date "start_date"
     t.date "finish_date"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
